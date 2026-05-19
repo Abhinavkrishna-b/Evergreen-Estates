bc5724e95bd481b87c0a38d499a65e2a4fe358d2
Abhinavkrishna-b <abhinavkrishna.b.2005@gmail.com>
2026-05-19 22:33:38 +0530

    feat: update PropertiesPage layout

--- frontend/src/routes/PropertiesPage/PropertiesPage.jsx
+++ frontend/src/routes/PropertiesPage/PropertiesPage.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
 import FilterBar from '../../components/FilterBar/FilterBar';
 import HorizontalCard from '../../components/HorizontalCard/HorizontalCard';
 import { featuredProperties } from '../../constants/properties';
@@ -94,6 +95,9 @@ const PropertiesPage = () => {
         </div>
 
       </div>
+
+      <Footer/>
+
     </div>
   );
 };
